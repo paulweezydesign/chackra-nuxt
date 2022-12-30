2d4d929ecaa080273eb0af9efa75636bc9bc2461
paulweezydesign <86060999+paulweezydesign@users.noreply.github.com>
2022-12-30 04:01:14 -0700

    Initial commit

---
+++ components/Drawer.vue
@@ -0,0 +1,52 @@
+<script>
+import {
+  CDrawer,
+  CDrawerBody,
+  CDrawerFooter,
+  CDrawerHeader,
+  CDrawerOverlay,
+  CDrawerContent,
+  CDrawerCloseButton,
+} from "@chakra-ui/vue";
+export default {
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    close() {
+      this.isOpen = false;
+    },
+  },
+};
+</script>
+<template>
+  <div>
+    <c-button ref="btnRef" @click="isOpen = true">Open Drawer</c-button>
+
+    <c-drawer
+      :isOpen="isOpen"
+      placement="right"
+      :on-close="close"
+      :initialFocusRef="() => $refs.inputInsideModal"
+    >
+      <c-drawer-overlay />
+      <c-drawer-content>
+        <c-drawer-close-button />
+        <c-drawer-header>Create your account</c-drawer-header>
+
+        <c-drawer-body>
+          <c-input ref="inputInsideModal" placeholder="Type here..." />
+        </c-drawer-body>
+
+        <c-drawer-footer>
+          <c-button variant="outline" mr="3" @click="isOpen = false"
+            >Cancel</c-button
+          >
+          <c-button variant-color="blue">Save</c-button>
+        </c-drawer-footer>
+      </c-drawer-content>
+    </c-drawer>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <CThemeProvider>
+      <CColorModeProvider>
+        <CBox font-family="body" as="main">
+          <CReset />
+          <nav class="">
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/about">About</NuxtLink>
+            <Drawer />
+          </nav>
+          <Nuxt />
+        </CBox>
+      </CColorModeProvider>
+    </CThemeProvider>
+  </div>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-evenly;
+  font-size: large;
+  padding-top: 1rem;
+}
+</style>
